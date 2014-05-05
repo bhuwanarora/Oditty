96c410af388fea23263c00a5a01d4a846f780433
abclearner2 <abclearner2@gmail.com>
2014-05-04 21:58:18 -0400

    sce error resolved

--- app/assets/javascripts/angular/controllers/search_controller.js
+++ app/assets/javascripts/angular/controllers/search_controller.js
@@ -1,4 +1,4 @@
-websiteApp.controller('searchController', function($scope, $rootScope, websiteService, $timeout){
+websiteApp.controller('searchController', function($scope, $rootScope, websiteService, $timeout, $sce){
 	_show_search_result = function(){
 		$rootScope.show_book = true;
 		$rootScope.book_x = 0;
