961b888c7a72a3f16cbd5b3771bcb88fa8c6912a
bhuwanarora67@gmail.com <bhuwanarora67@gmail.com>
2014-05-16 18:24:53 +0530

    an awesome search on the verge

--- app/assets/javascripts/angular/services/book_widget_service.js
+++
@@ -1,9 +0,0 @@
-websiteApp.service('bookWidgetService', function ($http, $q, $rootScope) {
-    this.populate_tooltips = function (id) {
-        var deferred = $q.defer();
-        $http.get('/api/v0/tooltip?id='+id).then(function(result) {
-            return deferred.resolve(result.data);
-        });
-        return deferred.promise;
-    };
-});
