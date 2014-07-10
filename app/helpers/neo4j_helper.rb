c00a4d6507d3faffcf3fecc503dbf4018d7e0da0
bhuwan <saurabhp0000@gmail.com>
2014-07-10 23:52:45 +0530

    neo4j config

--- app/helpers/neo4j_helper.rb
+++ app/helpers/neo4j_helper.rb
@@ -258,7 +258,7 @@ module Neo4jHelper
 	def self.init_goodreads_books_reverse
 		# count = GoodReadsBook.where(:neo_flag => true).count
 		t0 = Time.now
-		for id in 1350000..1599029
+		for id in 1460000..1599029
 			t1 = Time.now
 			begin
 				book = GoodReadsBook.find id
