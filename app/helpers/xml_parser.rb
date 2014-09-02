a994c1ed3678899f73e126fe5698a39d2c78a585
bhuwan <saurabhp0000@gmail.com>
2014-09-03 01:16:34 +0530

    phusion passenger bug

--- app/helpers/xml_parser.rb
+++ app/helpers/xml_parser.rb
@@ -1,13 +1,3 @@
 module XmlParser
-	def self.parse
-		test = ""
-		target = open('neo4j/out.graphml', 'w')
-		File.open('neo4j/out.graphml', 'r') do |f1| 
-			while line = f1.gets
-				puts line
-			end
-		open(name, 'wb') do |file|
-			file << open(Rails.root.join("neo4j/out.graphml")).read
-		end
-	end
+
 end
