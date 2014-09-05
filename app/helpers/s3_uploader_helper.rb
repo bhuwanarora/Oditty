d7f1ae28e9cfca822ae32cc15fdab104bce92339
bhuwan <saurabhp0000@gmail.com>
2014-09-05 16:47:25 +0530

    uploader id change

--- app/helpers/s3_uploader_helper.rb
+++ app/helpers/s3_uploader_helper.rb
@@ -125,7 +125,7 @@ module S3UploaderHelper
 	def self.get_images
 		@neo = Neography::Rest.new
 		skip = 1000
-		start_id = 387706 #MIN ID
+		start_id = 387851 #MIN ID
 		end_id = 2655796 #MAX ID
 		while start_id <= end_id
 			puts "get_images..."+start_id.to_s.green
