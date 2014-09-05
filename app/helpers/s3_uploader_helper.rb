e9db1788f7f978da30ddd15d39e9b4a9bc67570e
bhuwan <saurabhp0000@gmail.com>
2014-09-06 03:45:21 +0530

    uploader

--- app/helpers/s3_uploader_helper.rb
+++ app/helpers/s3_uploader_helper.rb
@@ -75,7 +75,7 @@ module S3UploaderHelper
 	def self.get_images2
 		@neo = Neography::Rest.new
 		skip = 1000
-		start_id = 1389500 #MIN ID 
+		start_id = 1389715 #MIN ID 
 		end_id = 2655796 #MAX ID
 		while start_id <= end_id
 			puts "get_images2..."+start_id.to_s.green
@@ -125,7 +125,7 @@ module S3UploaderHelper
 	def self.get_images
 		@neo = Neography::Rest.new
 		skip = 1000
-		start_id = 388733 #MIN ID
+		start_id = 388886 #MIN ID
 		end_id = 2655796 #MAX ID
 		while start_id <= end_id
 			puts "get_images..."+start_id.to_s.green
