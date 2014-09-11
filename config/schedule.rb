c04de6e610e2801cdf905216360963b63c849289
bhuwan <saurabhp0000@gmail.com>
2014-09-11 20:14:59 +0530

    test cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,5 +2,5 @@ set :output, "../dev/log/cron.log"
 
 every 1.minute  do
   # runner "TrendsHelper.social_mention"
-  runner "SubscriptionMailer.test_email"
+  runner "SubscriptionMailer.test_email.deliver"
 end
