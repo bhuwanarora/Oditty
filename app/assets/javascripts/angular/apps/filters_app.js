5c8cfb908651b975f2161da247894b14934e00f2
bhuwan <saurabhp0000@gmail.com>
2014-09-19 23:01:01 +0530

    font

--- app/assets/javascripts/angular/apps/filters_app.js
+++ app/assets/javascripts/angular/apps/filters_app.js
@@ -43,8 +43,8 @@ angular.module('filtersApp', [])
   .filter('reduced_title', function(){
     return function(input){
       if(angular.isDefined(input)){
-        if(input != null && input.length > 35){
-          input = input.slice(0, 32)+"...";
+        if(input != null && input.length > 32){
+          input = input.slice(0, 29)+"...";
         }
       }
       return input;
