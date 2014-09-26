2d8911d0d2db31173d69f71cec55546c0d4a7bb6
bhuwan <saurabhp0000@gmail.com>
2014-09-26 23:35:28 +0530

    icons

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,8 @@ module.exports = function(grunt) {
             'app/assets/stylesheets/min/card.min.css',
             'app/assets/stylesheets/min/grid.min.css',
             'app/assets/stylesheets/min/introjs-rtl.min.css',
-            'app/assets/stylesheets/min/introjs.min.css'
+            'app/assets/stylesheets/min/introjs.min.css',
+            'app/assets/stylesheets/min/icons.min.css',
           ]
         }
       }
