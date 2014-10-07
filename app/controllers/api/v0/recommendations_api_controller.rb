529828daf5c0cdd84e8dae169baa2a5da2a85567
bhuwan <saurabhp0000@gmail.com>
2014-10-07 12:56:12 +0530

    fb books

--- app/controllers/api/v0/recommendations_api_controller.rb
+++ app/controllers/api/v0/recommendations_api_controller.rb
@@ -73,7 +73,10 @@ module Api
 				@neo = Neography::Rest.new
 				filter_type = (JSON.parse params[:q])["filter_type"]
 				filters = JSON.parse(params[:q])
-				user_id = params[:id] unless session[:user_id].present?
+				unless session[:user_id].present?
+					user_id = params[:id]
+					session[:user_id] = params[:id]
+				end
 				if filter_type == "BOOK"
 					reading_time_filter = filters["other_filters"][Constants::Time].present?
 					clause = "MATCH (u:User) WHERE ID(u)="+user_id.to_s+" RETURN "
