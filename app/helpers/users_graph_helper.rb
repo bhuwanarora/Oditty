936f201fc861ea5fa7547a37994c730668218355
bhuwan <saurabhp0000@gmail.com>
2014-11-04 00:17:55 +0530

    news feed

--- app/helpers/users_graph_helper.rb
+++ app/helpers/users_graph_helper.rb
@@ -220,13 +220,13 @@ module UsersGraphHelper
 		#FIXME get_news_feed_for_user
 		@neo ||= self.neo_init
 		skip_count = 0 unless skip_count.present?
-		get_all_ego_relations_through_me = " OPTIONAL MATCH p=(u)-[r:Ego*..]->(friend:User) "
+		get_all_ego_relations_through_me = " OPTIONAL MATCH p=(u)-[r:Ego*..1]->(friend:User) "
 		filter_relations_only_on_me = "WHERE all(r2 in relationships(p) WHERE r2.user_id="+user_id.to_s+") WITH friend "
 		get_feed_of_my_ego_friends = "MATCH (friend)-[:FeedNext*]->(feed) "
 		return_data = "RETURN labels(feed), feed, feed.timestamp ORDER BY toInt(feed.timestamp) DESC SKIP "+skip_count.to_s+" LIMIT 10 "
 		clause = _match_user(user_id) + get_all_ego_relations_through_me + filter_relations_only_on_me + get_feed_of_my_ego_friends + return_data
 		puts clause.blue.on_red
-		# @neo.execute_query clause
+		@neo.execute_query clause
 	end
 
 	def self.get_personal_feed(user_id, skip_count)
