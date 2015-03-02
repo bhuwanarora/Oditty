0082774ca3d570a1bee9019b3f52a3f404e9c2b6
satishck1992 <satishck1992@gmail.com>
2015-03-02 21:48:30 +0530

    Raw version of trends Helper done

--- app/helpers/trends_helper.rb
+++ app/helpers/trends_helper.rb
@@ -1,36 +1,46 @@
 module TrendsHelper
 
 	def self.social_mention
-		tags = []
 		@news_sources ||= self.get_news_sources
 		for news_source in @news_sources
 			news_links = self.get_news news_source["url"]
 			
 			for news_link in news_links
+				tags = []
+				 
 				
-				merge_clause = "MERGE (news:Trending), (region:Region)-[original_relation:From]->(trending_news)"
-				where_clause = " WHERE region.name =" + news_link["region"].to_s + " AND news.url = " + news_link["url"].to_s
-				with_clause_first = " WITH news, region, original_relation"
-				optional_match_clause = " OPTIONAL MATCH (news)-[relation:From*1.." + Constants::UniqueNewsCount.to_s + "]->(region:Region)"
-				with_clause_second = " WITH news, region, original_relation. relation"
+				merge_clause = "MERGE (fresh_news:News{url:" + news_link.to_s + "}) WITH fresh_news MERGE (region:Region{name: " + news_source["region"].to_s + "})-[stale_relation:HasNews]->(stale_news)"
+				with_clause = " WITH fresh_news, region, stale_relation, stale_news"
+				optional_match_clause = " OPTIONAL MATCH (fresh_news)<-[relation:HasNews*1.." + Constants::UniqueNewsCount.to_s + "]-(region:Region)"
 
-				create_conditional = " CASE relation = NULL THEN [1] ELSE [] END AS dummyarray
-				FOREACH (variable in dummyarray | MERGE (region)-[:From]->(news)-[:From]->(original_news) WITH relation DELETE relation  RETURN ID(news))"
-				clause = create_new_news_node = merge_clause + where_clause + with_clause_first + optional_match_clause + with_clause_second + create_conditional
-				news_id = neo.execute_query clause["data"][0]
-
+				create_conditional = " FOREACH(ignoreMe IN CASE WHEN relation is null THEN [1] ELSE [] END | MERGE (region)-[:HasNews]->(fresh_news)-[:HasNews]->(stale_news) DELETE stale_relation) RETURN ID(fresh_news)"
+				clause = create_new_news_node = merge_clause + with_clause + optional_match_clause + create_conditional
+				news_id = self.execute_query clause["data"][0].to_i
 				tags_topics = self.get_tags news_link
-				response["social_tags"].each do |social_tag|
-				if social_tag["importance"] == 1 then tags << social_tag["originalValue"] end
 				
+				response["social_tags"].each do |social_tag|
+					if social_tag["importance"] == 1 then tags << social_tag["originalValue"] end
+				end
 
-				self.map_region_to_news news_source["region"], news_id
-				self.map_news_with_topics news_id, response["topics"] 				
-				self.map_tags_to_news news_id, tags
-				self.set_tags_on_books(tags, news_link, news_source["region"]) 
-
+				if self.is_news_fresh news_id
+					self.map_region_to_news(news_source["region"], news_id)
+					self.map_news_with_topics(news_id, response["topics"]) 				
+					self.map_tags_to_news(news_id, tags)
+					self.map_tags_to_books(tags, news_link, news_source["region"])
+				end 
 			end
 		end
+	end
+
+	def self.is_news_fresh
+		clause = " MATCH (region:Region)-[:HasNews]->(news) WHERE ID(news) = " + news_id.to_s
+		data = self.execute_query clause["data"][0]
+		if data.blank?
+			response = false
+		else
+			response = true
+		end
+		response
 	end
 
 	def self.map_news_with_topics news_id, topic
@@ -45,23 +55,24 @@ module TrendsHelper
 
 	def self.map_tags_to_news news_id, tags
 		for tag in tags
-			merge_clause = " MERGE (news)-[:HasTags]->(tag:Tag{name: " + tag.to_s +"}) WHERE ID(news) = " + news_id.to_s
+			merge_clause = " MERGE (news)-[:HasTags]->(tag:Trending{name: " + tag.to_s + "}) WHERE ID(news) = " + news_id.to_s
 			self.execute_query merge_clause
 		end
 	end
 
 	def self.execute_query clause
 		neo = Neography::Rest.new
+		puts clause
 		response = neo.execute_query clause	
 	end
 
-	def self.set_tags_on_books results, news_link, region
+	def self.map_tags_to_books results, news_link, region
 	    for trend in results
 	    	begin
 	        	merge_clause = " MERGE (t:Trending{name: " + trend.to_s + "}) ON CREATE SET t.status = 1, t.name = " +  trend.to_s + ", t.timestamp=" + Time.now.to_i.to_s + ", t.url = " + news_link.to_s + ", t.location = " + region.to_s + " ON MATCH SET t.status = 1 WITH t "
 				self.map_books_to_news(merge_clause, trend)
 	    	rescue Exception => e
-	    		
+	    		debugger
 	    	end
 	    end
 	end
@@ -88,7 +99,7 @@ module TrendsHelper
 	end
 
 	def self.get_tags news_link
-		query = Constants::NLPService + news_link rescue debugger
+		query = Constants::NLPService + news_link 
 		uri = URI(query)
 		response = Net::HTTP.get(uri)
 	end
@@ -99,7 +110,7 @@ module TrendsHelper
 			map_clause = " START book=node:node_auto_index('indexed_title:\""+indexed_title+"\"') MERGE (t)-[:RelatedBooks]->(book) "
 			puts (merge_clause + map_clause).blue.on_red
 			begin
-	    		neo.execute_query (merge_clause + map_clause)
+	    		self.execute_query (merge_clause + map_clause)
 				puts ""
 			rescue Exception => e
 				puts e.to_s
