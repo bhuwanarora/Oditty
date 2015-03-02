eac91a17b433e31965232a1b6c189893efbd30d1
satishck1992 <satishck1992@gmail.com>
2015-03-02 13:46:49 +0530

    Added book mapper

--- app/helpers/trends_helper.rb
+++ app/helpers/trends_helper.rb
@@ -49,23 +49,27 @@ module TrendsHelper
 			      	if trend_index.present?
 			        	big_clause = " MERGE (t"+count.to_s+":Trending{indexed_trending_topic:\""+trend_index+"\"}) ON CREATE SET t"+count.to_s+".searched_words = \""+trend.gsub("\"", "").to_s+"\", t"+count.to_s+".status = 1, t"+count.to_s+".name=\""+trend+"\", t"+count.to_s+".timestamp="+Time.now.to_i.to_s+", t"+count.to_s+".title=\""+news.title.gsub("\"", "").to_s+"\", t"+count.to_s+".content=\""+news.content.gsub("\"", "").to_s+"\", t"+count.to_s+".url=\""+news.uri.to_s+"\", t"+count.to_s+".publisher_thumb=\""+publisher_thumb.to_s+"\", t"+count.to_s+".thumbnail_url=\""+thumb.thumbnail_uri.to_s+"\", t"+count.to_s+".thumb=\""+thumb.uri.to_s+"\", t"+count.to_s+".location=\""+news.location.to_s+"\", t"+count.to_s+".publisher=\""+news.publisher.to_s+"\", t"+count.to_s+".redirect_url=\""+news.redirect_uri.to_s+"\" ON MATCH SET t"+count.to_s+".status = 1 WITH t"+count.to_s
 			      	end
-					Google::Search::Book.new(:query => trend).each do |book|
-						indexed_title = book.title.downcase.gsub(" ", "").gsub(":", "").gsub("'", "").gsub("!", "").gsub("[", "").gsub("[", "").gsub("\"", "")
-						clause = " START book=node:node_auto_index('indexed_title:\""+indexed_title+"\"') CREATE UNIQUE (t"+count.to_s+")-[:RelatedBooks]->(book) "
-						puts (big_clause + clause).blue.on_red
-						begin
-				    		neo.execute_query (big_clause + clause)
-							puts ""
-						rescue Exception => e
-							puts e.to_s
-						end
-					end
+					self.map_books_to_news trend
 				end
 	    	rescue Exception => e
 	    		
 	    	end
 	      count = count + 1
 	    end
+	end
+
+	def self.map_books_to_news
+		Google::Search::Book.new(:query => trend).each do |book|
+			indexed_title = book.title.downcase.gsub(" ", "").gsub(":", "").gsub("'", "").gsub("!", "").gsub("[", "").gsub("[", "").gsub("\"", "")
+			clause = " START book=node:node_auto_index('indexed_title:\""+indexed_title+"\"') CREATE UNIQUE (t"+count.to_s+")-[:RelatedBooks]->(book) "
+			puts (big_clause + clause).blue.on_red
+			begin
+	    		neo.execute_query (big_clause + clause)
+				puts ""
+			rescue Exception => e
+				puts e.to_s
+			end
+		end
 	end
 
 	def self.link_new_books(trend_id, keywords)
