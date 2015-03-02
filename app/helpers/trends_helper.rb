d3aeaddbfdc2f3ba11dd82ed343de7c4c917e589
satishck1992 <satishck1992@gmail.com>
2015-03-02 13:59:14 +0530

    Changing to master

--- app/helpers/trends_helper.rb
+++ app/helpers/trends_helper.rb
@@ -1,9 +1,5 @@
 module TrendsHelper
 
-	require 'nokogiri'
-	require 'open-uri'
-	require 'google-search'
-
 	def self.social_mention
 		count = 0
 	    neo = Neography::Rest.new 
@@ -13,55 +9,22 @@ module TrendsHelper
 			news_links = self.get_news news_source["url"]
 			
 			for news_link in news_links
-				
-				merge_clause = "MERGE (news:Trending), (region:Region)-[original_relation:From]->(trending_news)"
-				where_clause = " WHERE region.name =" + news_link["region"].to_s + " AND news.url = " + news_link["url"].to_s
-				with_clause_first = " WITH news, region, original_relation"
-				optional_match_clause = " OPTIONAL MATCH (news)-[relation:From*1.." + Constants::UniqueNewsCount.to_s + "]->(region:Region)"
-				with_clause_second = " WITH news, region, original_relation. relation"
-
-				create_conditional = " CASE relation = NULL THEN [1] ELSE [] END AS dummyarray
-				FOREACH (variable in dummyarray | MERGE (region)-[:From]->(news)-[:From]->(original_news) WITH relation DELETE relation  RETURN ID(news))"
-				clause = create_new_news_node = merge_clause + where_clause + with_clause_first + optional_match_clause + with_clause_second + create_conditional
-				news_id = neo.execute_query clause["data"][0]
-
 				tags = self.get_tags news_link
-				self.set_tags_on_news tags, news_id
-				self.link_new_books tags 
+				self.set_tags_on_books tags 
 			end
 		end
 	end
 
-	def self.set_tags_on_news tags, news_id
-		for social_tags in tags["social_tags"]
-			social_tags.each do |social_tag|
-				if social_tag["importance"] == 1
-					merge_clause = " MERGE (news)-[:HasInformationOf]->(tag:Tag { value = " + social_tag["originalValue"] + "}) WHERE ID(news) = " + news_id.to_s
-					neo.execute_query merge_clause
-				end
-			end
-		end
-	end
-
-	def self.link_new_books(trend_id, keywords)
-		neo = Neography::Rest.new 
-		
-		clause = "MATCH (t:Trending)-[r:RelatedBooks]->(:Book) WHERE ID(t)="+trend_id.to_s+" DELETE r SET t.searched_words = \""+keywords.to_s+"\""
-		neo.execute_query clause
-
-		big_clause = "MATCH (t:Trending) WHERE ID(t)="+trend_id.to_s+" WITH t"
-
-		Google::Search::Book.new(:query => keywords).each do |book|
-			indexed_title = book.title.downcase.gsub(" ", "").gsub(":", "").gsub("'", "").gsub("!", "").gsub("[", "").gsub("[", "").gsub("\"", "").to_s
-			clause = " START book=node:node_auto_index('indexed_title:\""+indexed_title+"\"') CREATE UNIQUE (t)-[:RelatedBooks]->(book) "
-			puts (big_clause + clause).blue.on_red
-			begin
-	    		neo.execute_query (big_clause + clause)
-				puts ""
-			rescue Exception => e
-				puts e.to_s
-			end
-		end
+	def self.set_tags_on_books results
+	    for trend in results
+	    	begin
+	        	big_clause = " MERGE (t"+count.to_s+":Trending{indexed_trending_topic:\""+trend_index+"\"}) ON CREATE SET t"+count.to_s+".searched_words = \""+trend.gsub("\"", "").to_s+"\", t"+count.to_s+".status = 1, t"+count.to_s+".name=\""+trend+"\", t"+count.to_s+".timestamp="+Time.now.to_i.to_s+", t"+count.to_s+".title=\""+news.title.gsub("\"", "").to_s+"\", t"+count.to_s+".content=\""+news.content.gsub("\"", "").to_s+"\", t"+count.to_s+".url=\""+news.uri.to_s+"\", t"+count.to_s+".publisher_thumb=\""+publisher_thumb.to_s+"\", t"+count.to_s+".thumbnail_url=\""+thumb.thumbnail_uri.to_s+"\", t"+count.to_s+".thumb=\""+thumb.uri.to_s+"\", t"+count.to_s+".location=\""+news.location.to_s+"\", t"+count.to_s+".publisher=\""+news.publisher.to_s+"\", t"+count.to_s+".redirect_url=\""+news.redirect_uri.to_s+"\" ON MATCH SET t"+count.to_s+".status = 1 WITH t"+count.to_s
+				self.map_books_to_news trend
+	    	rescue Exception => e
+	    		
+	    	end
+	      count = count + 1
+	    end
 	end
 
 	def self.get_news news_source
@@ -86,9 +49,13 @@ module TrendsHelper
 	end
 
 	def self.get_tags news_link
+		tags = []
 		query = Constants::NLPService + news_link rescue debugger
 		uri = URI(query)
 		response = Net::HTTP.get(uri)
+		response["social_tags"].each do |social_tag|
+		if social_tag["importance"] == 1 then tags << social_tag["originalValue"] end
+		tags
 	end
 
 	def self.map_news_to_book
@@ -104,4 +71,5 @@ module TrendsHelper
 			end
 		end
 	end
+
 end
