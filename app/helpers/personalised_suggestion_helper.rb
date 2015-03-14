ce59eff0b9fcb4782f7774c291e9aa99090c9ec8
satishck1992 <satishck1992@gmail.com>
2015-03-14 11:07:00 +0530

    Integrated personalised recommendation helpers

---
+++ app/helpers/personalised_suggestion_helper.rb
@@ -0,0 +1,206 @@
+module PersonalisedSuggestionHelper
+
+	def self.bookmark_book(user_id, book_id, bookmark_name)
+		#FIXME: bookmark book
+
+		@neo ||= self.neo_init
+		bookmark_clause = _match_user_and_book(user_id, book_id) + " WITH user, book MERGE (user)-[labelled:Labelled]->(label:Label{name: \"" + bookmark_name.strip.upcase + "\"}), (label)-[:BookmarkedOn]->(bookmark: BookmarkNode{label:\"" + bookmark_name.strip.upcase + "\", book_id:" + book_id.to_s + ", user_id:" + user_id.to_s + "}), (bookmark)-[:BookmarkAction]->(book) SET bookmark.title = book.title,  bookmark.author = book.author_name, bookmark.name = user.name, bookmark.email = user.email, bookmark.isbn = book.isbn, bookmark.timestamp = " + Time.now.to_i.to_s + ", bookmark.thumb = CASE WHEN user.thumb IS NULL THEN '' ELSE user.thumb END WITH user, book, bookmark, label, labelled "
+
+		feednext_clause = _feednext_clause(user_id)+", label, labelled "
+
+		bookfeed_next_clause = _bookfeed_clause(user_id)+", label, labelled "
+
+		existing_ego_clause = _existing_ego_clause+", label, labelled "
+
+		ego_clause = _ego_clause + ", label, labelled "
+
+		
+		set_clause = "SET book.bookmark_count = CASE WHEN book.bookmark_count IS NULL THEN 1 ELSE toInt(book.bookmark_count) + 1 END, user.bookmark_count = CASE WHEN user.bookmark_count IS NULL THEN 1 ELSE toInt(user.bookmark_count) + 1 END, label.bookmark_count = CASE WHEN label.bookmark_count IS NULL THEN 1 ELSE toInt(label.bookmark_count) + 1 END, labelled.bookmark_count = CASE WHEN labelled.bookmark_count IS NULL THEN 1 ELSE toInt(labelled.bookmark_count) + 1 END, user.total_count = CASE WHEN user.total_count IS NULL THEN " + Constants::BookmarkPoints.to_s + " ELSE toInt(user.total_count) + " + Constants::BookmarkPoints.to_s + " END"
+
+		update_root_category_like_clause = _get_book_left_a_mark_on_you_clause bookmark_name
+
+		clause = bookmark_clause + feednext_clause + bookfeed_next_clause + existing_ego_clause + ego_clause + set_clause + update_root_category_like_clause
+		puts clause.blue.on_red
+		debugger
+		puts "BOOK BOOKMARKED".green
+		@neo.execute_query(clause)
+		#update bookmark cache for the book
+		#update popularity index for the book
+		#update popularity index for the author
+
+		#update bookmark cache for the user
+		#update bookworm index for the user
+
+		#update news feed for the book
+		#update news feed for the user
+	end
+
+	private
+
+	def self.get_books_from_unexplored_subjects
+		get_books_from_unexplored_subjects_clause = " MATCH (user)-[likes_category:LikesCategory]->(root_category:Category{is_root:true}) WITH TOFLOAT(SUM(likes_category.likeability_index)*1.0/COUNT(root_category)) AS mean_likeability_index, user MATCH (user)-[likes_category:LikesCategory]->(root_category:Category{is_root:true}) WITH likes_category, root_category RETURN root_category, TOFLOAT(likes_category.likeability_index*1.0 - mean_likeability_index) AS mean_deviation ORDER BY mean_deviation  "
+		_match_user 
+	end
+
+	def self.get_top_ten_tiny_reads
+		get_top_ten_tiny_reads_clause = " MATCH path = (book)-[:NextTinyRead*9]->(tiny_reads) WHERE ID(book) = " + Constants::BestTinyRead.to_s + " RETURN EXTRACT (n IN nodes(path)|n) AS tiny_books "
+	end
+
+	def _get_book_left_a_mark_on_you_clause bookmark_name
+		bookmark_name.to_s.strip.upcase!
+		
+		update_root_category_likes_clause = " WITH user, label, bookmark, book OPTIONAL MATCH (user)-->(label)-->(bookmark)-->(book) WITH user, COUNT(DISTINCT book) as total_book_count MATCH (user)-->(label)-->(bookmark)-->(book)-[:FromCategory]->()-[:HasRoot*0..1]->(root_category{is_root:true}) WITH total_book_count, user, root_category MERGE (user)-[likes_category:LikesCategory]->(root_category) ON CREATE SET likes_category.weight = 1 ON MATCH SET likes_category.weight = likes_category.weight + 1 WITH user, total_book_count, root_category OPTIONAL MATCH  (user)-[likes_category:LikesCategory]->(root_category) WITH user, likes_category, root_category, total_book_count MATCH (user)-[likes_category:LikesCategory]->(root_category) SET likes_category.likeability_index = TOFLOAT(likes_category.weight*1.0/total_book_count)"
+									
+		set_favourite_clause = ", likes_category.favourite = true"
+		
+		case bookmark_name
+
+		when "BOOKSLEFTAMARKONYOU"
+			clause = update_root_category_likes_clause + set_favourite_clause
+		when "FROMFACEBOOK"
+			clause = update_root_category_likes_clause
+		when "MARKASREAD"
+			clause = update_root_category_likes_clause
+		else
+			clause = ""
+		end
+
+		clause	
+	end
+
+	def self.get_max_likeability_bookmark
+		get_max_likeabillity = " MATCH (user)-[likes_category:LikesCategory]->(root_category:Category{is_root:true}) WITH TOFLOAT(SUM(likes_category.likeability_index)*1.0/COUNT(root_category)) AS mean_likeability_index, user MATCH (user)-[likes_category:LikesCategory]->(root_category:Category{is_root:true}) WITH likes_category, root_category RETURN root_category, TOFLOAT(likes_category.likeability_index*1.0 - mean_likeability_index) AS mean_deviation ORDER BY mean_deviation  "
+		clause = _match_user + get_max_likeabillity
+	end
+
+	def self.get_most_read_author
+		get_max_read_author_clause = " MATCH (user)-->(:BookmarkNode)-->(book:Book)<-[:Wrote]-(author:Author) WITH author, COUNT(book) RETURN author ORDER BY books "
+		clause = _match_user + get_max_read_author_clause
+	end
+
+	def self.get_most_read_era
+		get_max_read_author_clause = " MATCH (user)-->(:BookmarkNode)-->(book:Book)-[::Published_in]-()-[:FromEra]->(era:Era) WITH era, COUNT(book) AS books_from_era RETURN era ORDER BY books_from_era LIMIT 1 "
+		clause = _match_user + get_max_read_era_clause
+	end
+
+	def self.books_on_your_friends_shelves
+		get_friends_reads_clause = "(User)-[:Follows]->(friend)-->(:Label{name:CurrentlyReading})-->(:BookmarkNode)--(book:Book) WITH COUNT(book) AS book_count, book RETURN book, book_count ORDER BY book_count DESC"
+		clause = _match_user + get_friends_reads_clause
+	end
+
+	def _match_user(user_id)
+		"MATCH (user:User) WHERE ID(user)="+user_id.to_s+" WITH user"
+	end
+
+	def _match_user_and_book(user_id, book_id)
+		"MATCH (user:User), (book:Book) WHERE ID(user) = " + user_id.to_s + " AND ID(book) = " + book_id.to_s + " "
+	end
+
+	def _recommend_clause(user_id, book_id, friend_id)
+		match_clause = "MATCH (u:User), (b:Book), (friend:User) "
+		where_clause = "WHERE ID(u)="+user_id.to_s+
+						" AND ID(b)="+book_id.to_s+
+						" AND ID(friend)="+friend_id.to_s+" "
+		create_clause = "CREATE UNIQUE (u)-[:RecommendedTo]->(friend)-[:RecommendedAction]->(m:RecommendNode{book_id:"+book_id.to_s+", title:b.title, author:b.author_name, user_id:"+user_id.to_s+", friend_id:"+friend_id.to_s+", timestamp:"+Time.now.to_i.to_s+", email:u.email, friend_email:friend.email})-[:Recommended]->(b) "
+		set_notification_label = "SET m :Notification "
+		set_clause = "SET m.name=u.name, m.friend_name=friend.name, m.isbn=b.isbn, m.thumb = CASE WHEN u.thumb IS NULL THEN '' ELSE u.thumb END WITH u, b, m "
+		clause = match_clause + where_clause + create_clause + set_notification_label + set_clause
+		clause
+	end
+
+	def _delete_existing_feednext_clause user_id
+		match_clause = "OPTIONAL MATCH (x)-[r1:FeedNext{user_id:"+user_id.to_s+"}]->(m)-[r2:FeedNext{user_id:"+user_id.to_s+"}]->(y) "
+		delete_clause = "FOREACH (a IN CASE WHEN x IS NULL THEN [] ELSE [x] END | FOREACH (b IN CASE WHEN y IS NULL THEN [] ELSE [y] END |	CREATE  (a)-[:FeedNext{user_id:"+user_id.to_s+"}]->(b) DELETE r1, r2)) "
+		with_clause = "WITH u, b, m "
+		clause = match_clause + delete_clause + with_clause
+		clause
+	end
+
+	def _feednext_clause(user_id, without_book=false)
+		find_old_feed = "MATCH (user)-[old:FeedNext]->(old_feed) "
+		create_new_feed = "CREATE UNIQUE (user)-[:FeedNext{user_id:"+user_id.to_s+"}]->(bookmark)-[:FeedNext{user_id:"+user_id.to_s+"}]->(old_feed) "
+		if without_book
+			delete_old_feed = "DELETE old WITH user, bookmark "
+		else
+			delete_old_feed = "DELETE old WITH user, book, bookmark "
+		end
+		clause = find_old_feed + create_new_feed + delete_old_feed
+		clause
+	end
+
+	def _bookfeed_clause user_id
+		find_old_book_feed = "MATCH (book)-[old:BookFeed]->(old_feed) "
+		create_new_book_feed = "CREATE UNIQUE (book)-[:BookFeed{user_id:" + user_id.to_s + "}]->(bookmark)-[:BookFeed{user_id:" + user_id.to_s + "}]->(old_feed) "
+		delete_old_book_feed = "DELETE old WITH user, book, bookmark "
+		clause = find_old_book_feed + create_new_book_feed + delete_old_book_feed
+		clause
+	end
+
+	def _existing_ego_clause(without_book=false)
+		find_friends = "OPTIONAL MATCH (user)<-[:Follow]-(friend:User) "
+		find_me_in_friends_ego_chain = "OPTIONAL MATCH (x1)-[r1:Ego{user_id:ID(friend)}]->(user)-[r2:Ego{user_id:ID(friend)}]->(x2) "
+		remove_me_from_friends_ego_chain = "FOREACH (s IN CASE WHEN r1 IS NULL THEN [] ELSE [r1] END | FOREACH (t IN CASE WHEN r2 IS NULL THEN [] ELSE [r2] END | CREATE (x1)-[:Ego{user_id:ID(friend)}]->(x2) DELETE s, t)) "
+		if without_book
+			with_clause = "WITH user, friend "
+		else
+			with_clause = "WITH user, book, friend "
+		end
+		clause = find_friends + find_me_in_friends_ego_chain + remove_me_from_friends_ego_chain + with_clause
+		clause
+	end
+
+	def _ego_clause(without_book=false)
+		friends_old_ego_clause = "OPTIONAL MATCH (friend)-[old:Ego{user_id:ID(friend)}]->(old_ego) "
+		create_new_and_delete_old_ego_clause = "FOREACH(p IN CASE WHEN old_ego IS NULL THEN [] ELSE [old_ego] END | FOREACH (q IN CASE WHEN friend IS NULL THEN [] ELSE [f] END | CREATE (q)-[:Ego{user_id:ID(q)}]->(user)-[:Ego{user_id:ID(q)}]->(p) DELETE old)) "
+		if without_book
+			with_clause = "WITH DISTINCT user "
+		else
+			with_clause = "WITH DISTINCT user, book "
+		end
+		clause = friends_old_ego_clause + create_new_and_delete_old_ego_clause + with_clause
+		clause
+	end
+
+	def _delete_feed_clause(user_id, other_label=nil)
+		if other_label
+			get_feed_relations_for_m = "MATCH (s)-[f1:FeedNext{user_id:"+user_id.to_s+"}]->("+other_label+")-[f2:FeedNext{user_id:"+user_id.to_s+"}]->(e) "
+		else
+			get_feed_relations_for_m = "MATCH (s)-[f1:FeedNext{user_id:"+user_id.to_s+"}]->(m)-[f2:FeedNext{user_id:"+user_id.to_s+"}]->(e) "
+		end
+		create_new_relations_in_place = "CREATE (s)-[:FeedNext{user_id:"+user_id.to_s+"}]->(e) "
+		delete_old_relations = "DELETE f1, f2 "
+		with_clause = "WITH u, b, m "
+		clause = get_feed_relations_for_m + create_new_relations_in_place + delete_old_relations + with_clause
+		clause
+	end
+
+	def _delete_book_clause(user_id, other_label=nil)
+		if other_label
+			get_bookfeed_relations_for_m = "MATCH (s)-[b1:BookFeed{user_id:"+user_id.to_s+"}]->("+other_label+")-[b2:BookFeed{user_id:"+user_id.to_s+"}]->(e) "
+		else
+			get_bookfeed_relations_for_m = "MATCH (s)-[b1:BookFeed{user_id:"+user_id.to_s+"}]->(m)-[b2:BookFeed{user_id:"+user_id.to_s+"}]->(e) "
+		end
+		create_new_relations_in_place = "CREATE (s)-[:BookFeed{user_id:"+user_id.to_s+"}]->(e) "
+		delete_old_relations = "DELETE b1, b2 "
+		with_clause = "WITH u, b, m"
+		clause = get_bookfeed_relations_for_m + create_new_relations_in_place + delete_old_relations + with_clause
+		clause
+	end
+
+	def _removing_rating
+		get_rating_relationship = "OPTIONAL MATCH (u)-[r1:RatingAction]->(rn:RatingNode)-[r2:Rate]->(b) "
+		delete_rating_relationship = "FOREACH (p IN CASE WHEN r1 IS NULL THEN [] ELSE [u] END | FOREACH (q IN CASE WHEN r2 IS NULL THEN [] ELSE [b] END | SET q.rating_count = CASE WHEN q.rating_count IS NULL THEN 0 ELSE toInt(q.rating_count) - 1 END,  p.total_count = CASE WHEN p.total_count IS NULL THEN 0 ELSE toInt(p.total_count) - " + Constants::RatingPoints.to_s + "  END, p.rating_count = CASE WHEN p.rating_count IS NULL THEN 0 ELSE toInt(p.rating_count) - 1 END DELETE r1, r2))  "
+		with_clause = "WITH u, b, m"
+		clause = get_rating_relationship + delete_rating_relationship + with_clause
+		clause
+	end
+
+	def _remove_timing
+		get_timing_relationship = "OPTIONAL MATCH (u)-[r1:TimingAction]->(tn:TimingNode)-[r2:Timer]->(b) "
+		delete_timing_relationship = "FOREACH (p IN CASE WHEN r1 IS NULL THEN [] ELSE [u] END | FOREACH (q IN CASE WHEN r2 IS NULL THEN [] ELSE [b] END | SET p.total_count = CASE WHEN p.total_count IS NULL THEN 0 ELSE toInt(p.total_count) - "+Constants::ReadTimePoints.to_s+" END DELETE r1, r2)) "
+		with_clause = "WITH u, b, m "
+		clause = get_timing_relationship + delete_timing_relationship + with_clause
+		clause
+	end
+
+end
---
+++ app/helpers/signup_book_finder_helper.rb
@@ -0,0 +1,54 @@
+module SignupBookFinderHelper
+	def _get_user_clause 
+			general_clause = " OPTIONAL MATCH (user:User)--(:MarkAsReadNode)--(book:Book) WHERE ID(user) = " + @user_id.to_s + "  WITH user, book"
+	end
+	def find_books_on_count @user_id, skip_count
+		begin
+			limit = 30
+
+			count = self.get_user_book_count
+
+			skip_count = skip_count.to_i > 30 ? skip_count.to_i : 0  
+
+			if range == ["0-20"] 
+
+				user_case_clause = " RETURN ID(book) ORDER BY book.total_weight"
+
+				clause = _get_user_clause + user_case_clause
+
+				data = (neo.execute_query clause)["data"].flatten
+
+				if data.blank?
+					
+					get_ten_small_books_clause = " MATCH initial_path = (initial_small_read_book:Book)-[:NextSmallRead*" + skip_count.to_s + "]-(small_read) WITH small_read MATCH small_read_path = (small_read)-[:NextSmallRead*30] WHERE ID(inital_small_read_book) = " + Constants::MostPopularSmallReadID + "  RETURN EXTRACT(n in nodes(small_read_path)|ID(n)) AS book_ids "
+
+					clause = get_user_book_count + get_ten_small_books_clause
+					data = (neo.execute_query clause)["book_ids"].flatten
+				end
+			else ["20-50", "50-100", "100-250"].include? range
+			 
+			 	match_book_genre_clause = "  MATCH book-[:FromCategory]->()-[HasRoot*0..1]->(root_category{is_root:true}) WITH book, root_category ORDER BY book.total_weight DESC LIMIT " + limit.to_s + " RETURN root_category.name, COLLECT(DISTINCT(book.title)) AS books, COUNT(book) AS book_count ORDER BY book_count DESC "
+
+			 	clause =  _get_user_clause + match_book_genre_clause
+			 	data = (neo.execute_query clause)["data"] 
+			 
+			 	if data.blank?
+			 		find_genre_books = "  MATCH (user:User)-[:Likes]->(category:Category)-[:HasRoot*0..1]->(root_category{is_root:true}) WITH root_category MATCH path = (root_category)-[:NextInGenre*" + skip_count.to_i.to_s + "]->(popular_books)  WITH EXTRACT(n IN nodes(path)| book) AS books UNWIND books AS book RETURN root_category, book ORDER BY book.total_weight SKIP " + skip_count.to_i.to_s + " LIMIT 30 "
+			 		clause = _get_user_clause + find_genre_books
+			 		
+			 		data = ((neo.execute_query clause)["data"])
+			 
+			 	end
+			end
+		rescue Exception => e
+			output = e.to_s
+			status = 500
+	end
+
+
+	def self.get_user_book_count 
+		count_match_clause = " MATCH (user:User) WHERE ID(user) = " + @user_id.to_s + " RETURN user.total_count"
+		range = ((neo.execute_query count_match_clause)["data"]).flatten.to_s
+		count = count.to_i
+	end
+end
