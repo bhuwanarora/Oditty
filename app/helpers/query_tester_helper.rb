0957c4d07235485772a924de0b60624c85fff6e4
satishck1992 <satishck1992@gmail.com>
2015-03-18 10:47:52 +0530

    functional wrapper

--- app/helpers/query_tester_helper.rb
+++
@@ -1,7 +0,0 @@
-module QueryTesterHelper
-	def self.return_user
-		neo = Neography::Rest.new
-		clause = " match (a:User) return a.name, a.password as name limit 4"
-		neo.execute_query clause
-	end
-end
