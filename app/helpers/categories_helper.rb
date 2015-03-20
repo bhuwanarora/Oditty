0e9b84784f66cd38bb70ab1a80f200b1fbb6c7de
satishck1992 <satishck1992@gmail.com>
2015-03-20 16:28:24 +0530

    Completed book categories in categoriesHelper

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,10 @@
 module CategoriesHelper
+	def self.get_categories book_id
+		neo = Neography::Rest.new
+		match_clause = " MATCH (book) WHERE ID(book) = " + book_id.to_s + " MATCH (book)-[:FromCategory]->(:Category)-[:HasRoot*0..1]->(root_category{is_root:true}) "
+		return_clause = " RETURN ID(root_category) AS id, root_category.icon AS icon, root_category.name AS name LIMIT 5"
+		clause = match_clause + return_clause
+		data = neo.execute_query clause
+		data
+	end
 end
