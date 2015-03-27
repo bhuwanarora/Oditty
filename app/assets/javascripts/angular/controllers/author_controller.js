958197ae7e8944d51710200a3976cd043ca2ee22
Bhuwan Arora <saurabhp0000@gmail.com>
2015-03-27 20:09:48 +0530

    author handing over to Anshul

--- app/assets/javascripts/angular/controllers/author_controller.js
+++ app/assets/javascripts/angular/controllers/author_controller.js
@@ -1,7 +1,7 @@
-homeApp.controller('authorController', ["$scope", 'parallaxHelper', function($scope, parallaxHelper){
+homeApp.controller('authorController', ["$scope", function($scope){
 
   	_init = function(){
-  		$scope.background_parallax = parallaxHelper.createAnimator(-0.3, 150, -150);
+  		// $scope.background_parallax = parallaxHelper.createAnimator(-0.3, 150, -150);
   	}
 
   	_init();
