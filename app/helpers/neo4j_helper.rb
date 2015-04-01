a2f129e7f9fe0fc6ca433030a5b19d95a3e65dc3
Bhuwan Arora <saurabhp0000@gmail.com>
2015-04-01 12:27:45 +0530

    community

--- app/helpers/neo4j_helper.rb
+++ app/helpers/neo4j_helper.rb
@@ -1142,7 +1142,7 @@ module Neo4jHelper
 			match_clause = " MATCH popularity_list = (book:Book)-[:Next_book*" + Constants::QueryStepDuringLinking.to_s + "]->(next_book) WHERE ID(book) = " + starting_book_id.to_s 
 			unwind_book_collection_clause = " WITH next_book AS least_popular_book_in_path, EXTRACT(n IN nodes(popularity_list)|n) AS books UNWIND books as book "
 			match_book_to_root_clause = " WITH least_popular_book_in_path, book MATCH (book)-[:FromCategory]->(category:Category)-[:HasRoot*0..1]->(root_category:Category{is_root:true}) "
-			get_last_linked_node_clause = "  WITH book, root_category, root_category.uuid AS uuid , least_popular_book_in_path OPTIONAL MATCH (root_category)<-[old:NextInCategory]-(last_node),(book)-[already_linked:NextInCategory{from_category:uuid}]-()   "
+			get_last_linked_node_clause = "  WITH book, root_category, root_category.uuid AS uuid , least_popular_book_in_path OPTIONAL MATCH (root_category)<-[old:NextInCategory]-(last_node) OPTIONAL MATCH (book)-[already_linked:NextInCategory{from_category:uuid}]-()   "
 			conditionally_make_relation_clause = " FOREACH (ignore IN CASE WHEN  already_linked IS NULL AND uuid IS NOT NULL THEN [old] ELSE [] END | MERGE (last_node)-[:NextInCategory{from_category:uuid}]->(book)-[:NextInCategory{from_category:uuid}]->(root_category)  DELETE old) RETURN ID(book) AS most_popular_book_id, ID(least_popular_book_in_path) AS least_popular_book_id  ORDER BY book.total_weight DESC LIMIT 1"
 			clause = match_clause + unwind_book_collection_clause + match_book_to_root_clause + get_last_linked_node_clause + conditionally_make_relation_clause
 			data = clause.execute
