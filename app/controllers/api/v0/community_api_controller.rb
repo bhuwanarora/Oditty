f21aa6eace391c5faae581082d65818f964bc601
satishck1992 <satishck1992@gmail.com>
2015-04-02 15:25:03 +0530

    Removed user unnecessary ids

--- app/controllers/api/v0/community_api_controller.rb
+++ app/controllers/api/v0/community_api_controller.rb
@@ -2,21 +2,18 @@ module Api
 	module V0
 		class CommunityApiController < ApplicationController
 			def get_books
-				user_id = session[:user_id]
 				community_id = params[:community_id]
 				info  = Api::V0::CommunityApi.get_books community_id
 				render :json => info, :status => 200
 			end
 
 			def get_users
-				community_id = params[:community_id]
 				user_id = session[:user_id]
 				info  = Api::V0::CommunityApi.get_users community_id
 				render :json => info, :status => 200
 			end
 
 			def get_communities_chronologically
-				community_id = params[:community_id]
 				user_id = session[:user_id]
 				info  = Api::V0::CommunityApi.get_communities_chronologically community_id
 				render :json => info, :status => 200
