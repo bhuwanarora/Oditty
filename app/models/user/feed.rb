4ecdfc7fc86be391e283db935b3f81b22ff73663
Bhuwan Arora <saurabhp0000@gmail.com>
2015-04-06 14:27:48 +0530

    incmplete feed on bookmakr

--- app/models/user/feed.rb
+++ app/models/user/feed.rb
@@ -30,6 +30,6 @@ class User::Feed < User
 	end
 
 	def create object
-		" MATCH (user)-[old:FeedNext]->(old_feed) CREATE UNIQUE (user)-[:FeedNext{user_id:"+@user_id.to_s+"}]->(" + object + ")-[:FeedNext{user_id:"+@user_id.to_s+"}]->(old_feed) DELETE old WITH user, "+object
+		" MATCH (user)-[old:FeedNext{user_id:"+@user_id.to_s+"}]->(old_feed) CREATE UNIQUE (user)-[:FeedNext{user_id:"+@user_id.to_s+"}]->(" + object + ")-[:FeedNext{user_id:"+@user_id.to_s+"}]->(old_feed) DELETE old WITH user, "+object+" "
 	end
 end
