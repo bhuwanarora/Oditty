afdd6e34485b382080a8a3104e9346badc157208
Satish <satishck1992@gmail.com>
2015-04-07 11:35:20 +0530

    removed functions

--- app/models/article/news.rb
+++ app/models/article/news.rb
@@ -7,14 +7,6 @@ class Article::News < Article
 		most_important_community + Community.match_grouped_books + Community.match_users + ", books_info WITH user, books_info , community " + User.collect_map({"users_info" => User.grouped_basic_info })  + " WITH users_info, books_info , community " + Community.collect_map({"most_important_tag" => Community.grouped_basic_info + ", books: books_info, users: users_info" })
 	end
 
-	def match_bookmark 
-		# " MATCH (user)-[labelled:Labelled]->(label:Label)-[bookmarked_on:BookmarkedOn]->(bookmark_node:BookmarkNode)-[bookmark_action:BookmarkAction]->(news:News) WHERE bookmark_node.user_id = " + @user_id.to_s + " "
-	end
-
-	def get_feed 
-		# " MATCH (news:News)-[:NewsFeed*0..]->(news_feed) WHERE ID(news) = " + @user_id.to_s + " RETURN labels(news_feed), news_feed "
-	end
-
 	def self.set_bookmark_count operation
 		if operation == "+"
 			" SET news.bookmark_count = TOINT(COALESCE(news.bookmark_count, 0)) + 1 "
@@ -41,10 +33,6 @@ class Article::News < Article
 
 	def self.basic_info
 		" ID(news) AS news_id, news.title AS title, news.published_year AS published_year "
-	end
-
-	def self.order_desc
-		# " ORDER BY news.total_weight DESC "
 	end
 
 	def get_chronological_news_info
