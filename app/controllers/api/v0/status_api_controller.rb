d3e820a802b183480876edc82a44d8ee7060300e
Bhuwan Arora <saurabhp0000@gmail.com>
2015-04-08 12:47:57 +0530

    Status

--- app/controllers/api/v0/status_api_controller.rb
+++ app/controllers/api/v0/status_api_controller.rb
@@ -4,8 +4,8 @@ module Api
 			def create
 				user_id = session[:user_id] 
 				status_info = JSON.parse params[:q] 
-				info = Api::V0::StatusApi.create(user_id, status_info).print
-				render :json => info, :status => 200
+				info = Api::V0::StatusApi.create(user_id, status_info).execute
+				render :json => {:message => "Success"}, :status => 200
 			end
 		end
 	end
