88660dd19cf9af14bccc2b9f3bc71bcef9772620
Satish <satishck1992@gmail.com>
2015-04-18 17:52:21 +0530

    Added labels in results for search

--- app/models/search.rb
+++ app/models/search.rb
@@ -7,43 +7,43 @@ class Search < Neo
 	end
 
 	def book_by_title
-		Search.match_indexed(Constant::IndexName::BookTitle, @params, @connector) + Search.return_group(Book.basic_info).search_compliant + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
+		Search.match_indexed(Constant::IndexName::BookTitle, @params, @connector) + Search.return_group(Book.basic_info,"labels(node) as labels").search_compliant + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
 	end
 
 	def author_by_name
-		Search.match_indexed(Constant::IndexName::MainAuthorName, @params, @connector) + Search.return_group(Author.basic_info).search_compliant + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
+		Search.match_indexed(Constant::IndexName::MainAuthorName, @params, @connector) + Search.return_group(Author.basic_info,"labels(node) as labels").search_compliant + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
 	end
 
 	def user_by_name
-		Search.match_indexed(Constant::IndexName::UserName, @params, @connector) + Search.return_group(User.basic_info).search_compliant + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
+		Search.match_indexed(Constant::IndexName::UserName, @params, @connector) + Search.return_group(User.basic_info,"labels(node) as labels").search_compliant + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
 	end
 
 	def category_by_name
 		if @params.present?
-			clause = Search.match_indexed(Constant::IndexName::CategoryName, @params, @connector) + Search.return_group(Category.basic_info).search_compliant + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
+			clause = Search.match_indexed(Constant::IndexName::CategoryName, @params, @connector) + Search.return_group(Category.basic_info,"labels(node) as labels").search_compliant + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
 		else
-			clause = Category::Root.match_root + Search.return_group(Category.basic_info)
+			clause = Category::Root.match_root + Search.return_group(Category.basic_info,"labels(node) as labels")
 		end
 		clause	
 	end
 
 	def news_by_title
-		Search.match_indexed(Constant::IndexName::NewsTitle, @params, @connector) + Search.return_group(News.basic_info).search_compliant + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
+		Search.match_indexed(Constant::IndexName::NewsTitle, @params, @connector) + Search.return_group(News.basic_info,"labels(node) as labels").search_compliant + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
 	end
 
 	def blog_by_title
-		Search.match_indexed(Constant::IndexName::BlogTitle, @params, @connector) + Search.return_group(Blog.basic_info).search_compliant + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
+		Search.match_indexed(Constant::IndexName::BlogTitle, @params, @connector) + Search.return_group(Blog.basic_info,"labels(node) as labels").search_compliant + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
 	end
 
 	def community_by_name
-		Search.match_indexed(Constant::IndexName::CommunityName, @params, @connector) + Search.return_group(Community.basic_info).search_compliant + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
+		Search.match_indexed(Constant::IndexName::CommunityName, @params, @connector) + Search.return_group(Community.basic_info,"labels(node) as labels").search_compliant + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
 	end
 
 	def basic
-		Search.match_indexed(Constant::IndexName::Search, @params, @connector) + Search.return_group(Search.basic_search_info) + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
+		Search.match_indexed(Constant::IndexName::Search, @params, @connector) + Search.return_group(Search.basic_search_info,"labels(node) as labels") + Search.skip(@count) + Search.limit(Constant::Count::ElementsShownInSearch)
 	end
 
 	def self.basic_search_info
-		" CASE WHEN node.title IS NULL THEN node.name ELSE node.title END as name, node.author_name as author_name, ID(node) as id, labels(node) as labels, node.first_name AS first_name, node.last_name AS last_name, node.image_url AS image_url, node.url AS url, node.blog_url AS blog_url  " 
+		" CASE WHEN node.title IS NULL THEN node.name ELSE node.title END as name, node.author_name as author_name, ID(node) as id, labels(node) as labels, node.first_name AS first_name, node.last_name AS last_name, node.image_url AS image_url, node.url AS url, node.blog_url AS blog_url, node.description AS description " 
 	end
 end
