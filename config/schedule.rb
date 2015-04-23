a0e3d1a92ec03f0eafee469830a48f458f6f7d61
Satish <satishck1992@gmail.com>
2015-04-23 17:23:14 +0530

    Testing configuration

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,23 +1,12 @@
-job_type :cronner, "cd :path && bundle exec rails runner -e :environment ':task' :output"
-job_type :cronner_rails, %Q{export PATH=/opt/rbenv/shims:/opt/rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; cd :path && bundle exec rake -e :environment ':task' :output}
 set :output, "log/cron.log"
 env :PATH, ENV['PATH']
-env :GEM_PATH, " /home/ubuntu/.rbenv/versions/2.0.0-p598/lib/ruby/gems/2.0.0"
 
 every 1.hour do
-  runner "News.handle"
+  rake " get_media:news "
 end
 
 every 1.day, :at => '6:05 pm' do
-  runner "Blog.handle"
-end
-
-every 1.minute  do
-  cronner " TesterHelper.outs "
-end
-
-every 1.minute  do
-  cronner_rails " TesterHelper.outs "
+  rake " get_media:blog "
 end
 
 every 1.minute  do
