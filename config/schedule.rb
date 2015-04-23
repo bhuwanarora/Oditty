5e7526c45e62f8beaeae6a226416bd04e977bf25
Satish <satishck1992@gmail.com>
2015-04-23 13:23:52 +0530

    Trying to resolve the deployment bug

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, "log/cron.log"
-# env :PATH, ENV['PATH']
-# env :GEM_PATH, ENV['GEM_PATH']
+env :PATH, ENV['PATH']
+env :GEM_PATH, " /home/ubuntu/.rbenv/versions/2.0.0-p598/lib/ruby/gems/2.0.0"
 every 1.hour do
   runner "News.handle"
 end
