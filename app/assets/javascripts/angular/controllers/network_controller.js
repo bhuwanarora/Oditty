02214ea97bbf841624873bec285aacb2925ac7d8
shashankvaibhav <shashank.vaibhav@outlook.com>
2015-04-24 19:48:06 +0530

    Edited Followers Following

--- app/assets/javascripts/angular/controllers/network_controller.js
+++ app/assets/javascripts/angular/controllers/network_controller.js
@@ -1,6 +1,7 @@
-homeApp.controller('networkController', ["$scope", "$rootScope", 'networkService', function($scope, $rootScope, networkService){
+homeApp.controller('networkController', ["$scope", "$rootScope", 'networkService', '$location', function($scope, $rootScope, networkService, $location){
 	$scope.get_followers = function(){
 		$scope.users_list = [];
+		$scope.selectedIndex=0;
 		networkService.get_followers().then(function(data){
 			$scope.users_list = data;
 		});
@@ -8,12 +9,26 @@ homeApp.controller('networkController', ["$scope", "$rootScope", 'networkService
 
 	$scope.get_users_followed = function(){
 		$scope.users_list = [];
+		$scope.selectedIndex=1;
 		networkService.get_users_followed().then(function(data){
 			$scope.users_list = data;
 		});	
 	}
 
 	var _init = (function(){
-		$scope.get_followers();
+	    var regex = /[?&]([^=#]+)=([^&#]*)/g;
+		var url_parser = regex.exec($location.absUrl());
+		if(angular.isDefined(url_parser) && url_parser != null){
+			var follow_state = url_parser[2];
+			if(follow_state == 0){
+               $scope.get_followers();
+			}
+			else if(follow_state == 1){
+               $scope.get_users_followed();
+
+		
+			}
+		}
 	}());
 }]);
+
