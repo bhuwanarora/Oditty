1fc4a2eb18bf1efc8c5164310676dde8c2a2c58a
Bhuwan Arora <saurabhp0000@gmail.com>
2015-04-26 04:12:04 +0530

    room

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,10 @@
 class Article < Neo
-end
 
+	def self.basic_info
+		" ID(article) AS id, article.title AS title, article.url AS URL, article.created_at AS created_at, article.description AS description, article.view_count AS view_count, article.image_url AS image_url"
+	end
+
+	def self.grouped_basic_info
+		" id: ID(article), title: article.title, url: article.url, created_at: article.created_at, description: article.description, view_count: article.view_count, image_url: article.image_url "
+	end
+end
--- app/models/bookmark/node/article_label.rb
+++ app/models/bookmark/node/article_label.rb
@@ -1,8 +1,7 @@
 class Bookmark::Node::ArticleLabel < Bookmark::Node
 
 	def self.get_public user_id
-		where_clause = " WHERE bookmark_node.public = true AND ( article:News  OR article:Blog) WITH user, labelled, label, bookmarked_on, bookmark_node, bookmark_action, article, COUNT(label) AS label_count "
-		Bookmark::Node::ArticleLabel.optional_match_user + where_clause + Bookmark::Node::ArticleLabel.return_group(Label.basic_info, Bookmark.basic_info, "COLLECT({"+Bookmark::Node::ArticleLabel.grouped_basic_info+"}) AS articles" )  + ", label_count " + Bookmark::Node::ArticleLabel.order_init + " label_count, created_at DESC " + Bookmark::Node::ArticleLabel.limit(Constant::Count::ArticlesShownInRoom)  
+		User.new(user_id).match + Label.match_public + Label.optional_match_articles + Bookmark::Node::ArticleLabel.return_group("DISTINCT label.name AS shelf", "article AS articles", "label_count AS label_count")
 	end
 
 	def self.get_visited
