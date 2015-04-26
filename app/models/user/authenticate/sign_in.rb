d791830ad040aa40cb9fe35c490892ae5dea0fce
Satish <satishck1992@gmail.com>
2015-04-26 08:41:51 +0530

    Removed check for active user

--- app/models/user/authenticate/sign_in.rb
+++ app/models/user/authenticate/sign_in.rb
@@ -10,16 +10,13 @@ class User::Authenticate::SignIn < User::Authenticate
 		user = (User.get_sign_in_credential_by_email(@params[:email]).execute)[0]
 		puts "signin".red
 		begin
-			active_user_authenticated = (user["password"] == @params[:password]) && user["verified"] && (user["active"] == true)
 			user_authenticated = user["password"] == @params[:password] && user["verified"]
-			if active_user_authenticated
+			if user_authenticated
 				authenticate = true
 				clause = User.new(user["id"]).match + User::Info.set_last_login(@params[:email])
 				clause.execute
 				info = {:profile_status => 0, :user_id => user["id"]}
 				message = Constant::StatusMessage::LoginSuccess
-			elsif user_authenticated
-				message = Constant::StatusMessage::PendingActivation
 			elsif  user["password"] != @params[:password]
 				message = Constant::StatusMessage::AuthenticationFailed
 			elsif !user["verified"]
