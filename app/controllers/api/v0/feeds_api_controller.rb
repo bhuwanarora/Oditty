a96c3df991a0141b3c412ec4f867e59f94b6e4a5
Satish <satishck1992@gmail.com>
2015-04-29 15:35:21 +0530

    Reset news relevant session values on new home landing

--- app/controllers/api/v0/feeds_api_controller.rb
+++ app/controllers/api/v0/feeds_api_controller.rb
@@ -15,16 +15,12 @@ module Api
 			end
 
 			def get_news  
-				if params["new_landing"] == "true"
-					session[:news_day_skip_count] = 0
-					session[:news_skip_count] = 0
-				end 
-				session[:news_skip_count] += Constant::Count::SkipNews
 				if (info = Api::V0::FeedsApi.get_news(session[:news_skip_count], session[:news_day_skip_count]).execute).blank?
 					session[:news_day_skip_count] += Constant::Count::SkipDays
 					session[:news_skip_count] = 0
 					info = Api::V0::FeedsApi.get_news(session[:news_skip_count], session[:news_day_skip_count]).execute
 				end
+				session[:news_skip_count] += Constant::Count::SkipNews
 				render :json => info, :status => 200
 			end
 
