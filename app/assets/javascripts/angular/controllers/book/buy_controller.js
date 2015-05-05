fecc1cdb54a50cc82ce8099f6b41c2560c56c7ce
Bhuwan Arora <saurabhp0000@gmail.com>
2015-05-05 13:38:30 +0530

    Author page scroll, buy

--- app/assets/javascripts/angular/controllers/book/buy_controller.js
+++ app/assets/javascripts/angular/controllers/book/buy_controller.js
@@ -1,11 +1,21 @@
-homeApp.controller('buyController', ["$scope", "$rootScope", "bookService", function($scope, $rootScope, bookService){
+homeApp.controller('buyController', ["$scope", "$rootScope", "bookService", "sharedService", function($scope, $rootScope, bookService, sharedService){
 	var _init = (function(){
 		var id = ($rootScope.active_book.id) || ($rootScope.active_book.book_id);
+		if(angular.isUndefined($scope.book)){
+			$scope.book = $rootScope.active_book;
+		}
 		$scope.book_loading = true;
 		bookService.get_borrow_users(id).then(function(data){
 			$scope.borrow_users = data;
 			$scope.book_loading = false;
 		});
 	}());
+
+	$scope.toggle_bookmark = function(status){
+		var id = ($rootScope.active_book.id) || ($rootScope.active_book.book_id);
+		var label = {"label_key": "IOwnThis"};
+		$rootScope.bookmark_object = {"type": "Book", "id": id};
+		sharedService.toggle_bookmark(label, status);
+	}
 
 }]);
