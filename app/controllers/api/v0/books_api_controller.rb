260f5259a6f9722817313b543836786c33971c4c
Satish <satishck1992@gmail.com>
2015-05-11 16:53:14 +0530

    removed a stupid bug

--- app/controllers/api/v0/books_api_controller.rb
+++ app/controllers/api/v0/books_api_controller.rb
@@ -44,7 +44,7 @@ module Api
 
 			def get_book_details
 				id = params[:id]
-				user_id = session[:user_i]
+				user_id = session[:user_id]
 				info = BookApi.get_book_details(id, user_id)
 				render :json => info, :status => 200
 			end
