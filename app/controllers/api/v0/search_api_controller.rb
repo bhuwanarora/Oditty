c7098d0595b61035b6a5f955235111c727fc890e
Satish <satishck1992@gmail.com>
2015-05-22 13:38:00 +0530

    Search index error resolved

--- app/controllers/api/v0/search_api_controller.rb
+++ app/controllers/api/v0/search_api_controller.rb
@@ -6,20 +6,26 @@ module Api
 				query_params = params[:q].strip
 				count = params[:count].to_i
 				type = params[:type]
-				puts query_params.to_s.white
-				puts session[:query].to_s.white
-				puts session[:query_type].to_s.white
 				puts type.to_s.white
-				unless session[:query] == query_params and session[:query_type] == type
+				if !(session[:query] == query_params and session[:query_type] == type) 
+					puts " search with params"
 					results = SearchApi.search(params)
 					session[:query] = query_params
 					session[:query_type] = type
 					session[:scroll_id] = results["scroll_id"] if results["scroll_id"].present?
 					results = results["results"]
-				else
-					results = SearchApi.search_by_scroll_id(session[:scroll_id])["results"]
+				elsif session[:scroll_id].present?
+					puts " search with scroll id "
+					results = SearchApi.search_by_scroll_id(session[:scroll_id])
 					session[:scroll_id] = results["scroll_id"] if (results.present? && results["scroll_id"].present?)
+					results = results["results"]
+				else
+					puts " search rest all "
+					results = SearchApi.search(params)["results"]
 				end
+				puts query_params.to_s.white
+				puts session[:query].to_s.white
+				puts session[:scroll_id].to_s.red rescue ""
 				puts results.to_s.green
 				render :json => results, :status => 200
 			end
