95eceb519e17b2da40decb3a845a5e707b3c8ada
Satish <satishck1992@gmail.com>
2015-05-23 18:05:15 +0530

    Root categories in infinity

--- app/models/infinity.rb
+++ app/models/infinity.rb
@@ -11,19 +11,20 @@ class Infinity < Neo
 
 	def get_books
 		return_group = []
+		with_clause = ""
 
 		only_read_time = !@category_id && !@author_id && !@era_id && @reading_time_id.present?
 		only_category = !@reading_time_id && !@author_id && !@era_id && @category_id.present?
 		if only_read_time
 			puts "only_read_time".green
 			puts @skip_count
-			clause = ReadTime.new(@reading_time_id).match_books_after(@skip_count, Limit) + Infinity.return_group(Infinity.collect_map({"books" => Book.grouped_basic_info}))
+			clause = ReadTime.new(@reading_time_id).match_books_after(@skip_count, Limit) + Infinity.match_categories + Infinity.return_group(Infinity.collect_map({"books" => "#{Book.grouped_basic_info}, root_categories: root_categories"}))
 		elsif only_category
+			with_clause += " ,category "
 			puts "only_category".green
-			clause = Infinity::FilterCategory.new(@category_id).get_books(@skip_count, Limit)
+			clause = Infinity::FilterCategory.new(@category_id).get_books(@skip_count, Limit) + Infinity.match_categories(with_clause) +  Infinity::FilterCategory.return_group(Category.basic_info, Infinity::FilterCategory.collect_map({"books" => "#{Book.grouped_basic_info}, root_categories: root_categories "}))
 		else
 			clause = ""
-			with_clause = ""
 			book_label_defined = false
 			if @author_id.present?
 				clause += Infinity::FilterAuthor.new(@author_id).match 
@@ -48,15 +49,18 @@ class Infinity < Neo
 				clause += Infinity::FilterEra.new(@era_id).match + with_clause
 			end
 
-			clause += Category::Root.optional_match_books_root +  with_clause + " WITH COLLECT (DISTINCT " + Category.grouped_basic_info("root_category") + ") AS categories, book " + with_clause 
-			with_clause += ", categories "
+			clause += Infinity.match_categories(with_clause)
 
 			if return_group.present?
-				clause += Book.order_desc + Infinity.skip(@skip_count) + Infinity.limit(Limit) + Infinity.return_group(Infinity.collect_map({"books" => "#{Book.grouped_basic_info}, categories: categories"}),return_group) 
+				clause += Book.order_desc + Infinity.skip(@skip_count) + Infinity.limit(Limit) + Infinity.return_group(Infinity.collect_map({"books" => "#{Book.grouped_basic_info}, root_categories: root_categories"}),return_group) 
 			else
-				clause += Book.order_desc + Infinity.skip(@skip_count) + Infinity.limit(Limit) + Infinity.return_group(Infinity.collect_map({"books" => Book.grouped_basic_info})) 
+				clause += Book.order_desc + Infinity.skip(@skip_count) + Infinity.limit(Limit) + Infinity.return_group(Infinity.collect_map({"books" => "#{Book.grouped_basic_info}, root_categories: root_categories"})) 
 			end
 		end
 		clause
+	end
+
+	def self.match_categories (with_clause="")
+		Category::Root.optional_match_books_root +  with_clause + " WITH COLLECT (DISTINCT " + Category.grouped_basic_info("root_category") + ") AS root_categories, book " + with_clause
 	end
 end
