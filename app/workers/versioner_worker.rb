35bdb99ae988f5757be96f33fc1ab51585b6f5ef
Satish <satishck1992@gmail.com>
2015-05-25 21:00:23 +0530

    corrected versioner

--- app/workers/versioner_worker.rb
+++ app/workers/versioner_worker.rb
@@ -6,11 +6,11 @@ class VersionerWorker
 			type.downcase!
 			case type
 			when "user"
-				url = "#{Rails.application.config.image_service}/api/v0/user_versions_update?id=#{id}&&bucket=#{Rails.application.config.user_bucket}&&url=#{url}}"		
+				url = "#{Rails.application.config.image_service}/api/v0/user_versions_update?id=#{id}&&bucket=#{Rails.application.config.user_bucket}&&url=#{url}"		
 			when "news"
-				url = "#{Rails.application.config.image_service}/api/v0/news_versions?id=#{id}&&bucket=#{Rails.application.config.news_bucket}&&url=#{url}}"		
+				url = "#{Rails.application.config.image_service}/api/v0/news_versions?id=#{id}&&bucket=#{Rails.application.config.news_bucket}&&url=#{url}"		
 			when "community"
-				url = "#{Rails.application.config.image_service}/api/v0/community_versions?id=#{id}&&bucket=#{Rails.application.config.community_bucket}&&url=#{url}}"		
+				url = "#{Rails.application.config.image_service}/api/v0/community_versions?id=#{id}&&bucket=#{Rails.application.config.community_bucket}&&url=#{url}"		
 			end
 			JSON.parse(Net::HTTP.get(URI.parse(URI.encode(url))))
 		end
