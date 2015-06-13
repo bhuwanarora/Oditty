d991fe7614245896bc8f93270a0663c77c8e41d5
Bhuwan Arora <saurabhp0000@gmail.com>
2015-06-13 13:41:15 +0530

    design 2.0

--- config/routes/book_api_routes.rb
+++ config/routes/book_api_routes.rb
@@ -7,7 +7,8 @@ ReadersDoor::Application.routes.draw do
 
       match "get_similar_books"                      => 'books_api#get_similar_books',                  :via => [:put, :get, :post]
       match "book"                                   => 'books_api#get_book_details',                   :via => [:put, :get, :post]
-      match "basic_book"                             => 'books_api#get_basic_book_details',             :via => [:put, :get, :post]
+      match "basic_book"                             => 'books_api#get_basic_feed_info',                :via => [:put, :get, :post]
+      match "book_primary_info"                      => 'books_api#get_primary_info',                   :via => [:put, :get, :post]
       match "book_feed"                              => 'books_api#get_feed',                           :via => [:put, :get, :post]
       match "tooltip"                                => 'books_api#tooltip',                            :via => [:put, :get, :post]
       match "moments"                                => 'books_api#moments',                            :via => [:put, :get, :post]
