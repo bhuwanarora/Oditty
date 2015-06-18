539537576b9297885b1cc717d21b33e4085fbede
Bhuwan Arora <saurabhp0000@gmail.com>
2015-06-18 18:16:13 +0530

    fb books

--- app/assets/javascripts/angular/controllers/signup_controller.js
+++ app/assets/javascripts/angular/controllers/signup_controller.js
@@ -202,7 +202,7 @@ homeApp.controller('signupController', ["$scope", "$rootScope", "Facebook", "$ti
             Facebook.api('me/books.reads', function(response){
                 websiteService.handle_facebook_books(response);
             });
-            Facebook.api('me/books.wants_to_reads', function(response){
+            Facebook.api('me/books.wants_to_read', function(response){
                 websiteService.handle_facebook_books(response);
             });
         });
