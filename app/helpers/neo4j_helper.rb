5d64e5d5f2371602648170b0dce565837df7c71c
Bhuwan Arora <saurabhp0000@gmail.com>
2015-06-18 11:33:29 +0530

    create_linked_list

--- app/helpers/neo4j_helper.rb
+++ app/helpers/neo4j_helper.rb
@@ -776,9 +776,12 @@ module Neo4jHelper
 
 	def create_linked_list
 		@neo ||= self.init
+		clause = " MATCH ()-[r:NextBook]->() DELETE r "
+		clause.execute
+
 		clause = "MATCH (book:Book) WITH book, toFloat(book.gr_rating)*toFloat(book.gr_ratings_count)*toFloat(book.gr_reviews_count) as weight ORDER BY weight DESC, toFloat(book.gr_rating) WITH collect(book) as p FOREACH(i in RANGE(0, length(p)-2) |  FOREACH(p1 in [p[i]] |  FOREACH(p2 in [p[i+1]] |  CREATE UNIQUE (p1)-[:NextBook]->(p2)))) WITH LAST(p) AS last, HEAD(p) AS head MERGE (last)-[:NextBook]->(head)"
 		puts "adding books in form of sorted linked lists...".green
-		@neo.execute_query clause
+		clause.execute
 	end
 
 	def self.restructure_database
