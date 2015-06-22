3afb88e7c90e4e30bb97a70d746ee655f9fb8932
Satish <satishck1992@gmail.com>
2015-06-22 15:28:16 +0530

    added start time and publish time

--- app/helpers/facebook_books_helper.rb
+++ app/helpers/facebook_books_helper.rb
@@ -8,7 +8,7 @@ module FacebookBooksHelper
 			for data in books
 				book = data["data"]["book"] 
 				if data["application"]["id"] == facebook_app_id
-					book_id = FacebookBooksHelper.handle_facebook_book(book, user_id) 
+					book_id = FacebookBooksHelper.handle_facebook_book(data, user_id) 
 					FacebookBooksHelper.set_bookmark("book.from_facebok", user_id, book_id)
 				elsif data["application"]["id"] == goodreads_app_id
 					book_id = FacebookBooksHelper.handle_gr_book(data, user_id) 
@@ -21,7 +21,7 @@ module FacebookBooksHelper
 	def self.handle_gr_book data, user_id
 		book = data["data"]["book"] 
 		progress = data["data"]["progress"]
-		reading_journey_info = (FacebookBook.merge_by_gr_url(book["url"]) + ReadingJourney.link_reading_journey(user_id) + Book.return_group("COALESCE(recent_status.timestamp,0) AS timestamp, ID(reading_journey) AS id , ID(book) AS book_id")).execute[0]
+		reading_journey_info = (FacebookBook.merge_by_gr_url(book["url"]) + ReadingJourney.link_reading_journey(user_id) + ReadingJourney.set_publish_time(Time.parse(data["publish_time"]).to_i) + ReadingJourney.set_start_time(Time.parse(data["start_time"]).to_i) + Book.return_group("COALESCE(recent_status.timestamp,0) AS timestamp, ID(reading_journey) AS id , ID(book) AS book_id")).execute[0]
 		book_id = reading_journey_info['book_id']
 		progress_link = ReadingJourney.create_progress(reading_journey_info, progress)
 		if progress_link.present?
@@ -30,9 +30,10 @@ module FacebookBooksHelper
 		book_id
 	end
 
-	def self.handle_facebook_book book, user_id
+	def self.handle_facebook_book data, user_id
+		book = data["data"]["book"] 
 		facebook_id = book["id"]
-		(FacebookBook.new(facebook_id).merge(book) + ReadingJourney.link_reading_journey(user_id) + Book.return_group(Book.basic_info)).execute[0]['book_id'] 
+		(FacebookBook.new(facebook_id).merge(book) + ReadingJourney.link_reading_journey(user_id) + ReadingJourney.set_publish_time(Time.parse(data["publish_time"]).to_i) + ReadingJourney.set_start_time(Time.parse(data["start_time"]).to_i) + Book.return_group(Book.basic_info)).execute[0]['book_id'] 
 	end
 
 	def self.set_bookmark type, user_id, book_id
