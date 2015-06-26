cf2558dd34b19044b587cf0f4fe0e52bc4ea32ab
ashesh-0 <ashesh276@gmail.com>
2015-06-26 10:53:16 +0530

    braces in string taken care of.

--- app/helpers/book_helper.rb
+++ app/helpers/book_helper.rb
@@ -11,7 +11,7 @@ module BookHelper
 
 	def self.get_by_one_author book_name, author_name_list
 		replace_dictionary = {"@" => "", "."  => ""}
-		clause = " START books = node:node_auto_index('indexed_title: " + book_name.search_ready + "') WITH books "
+		clause = " START books = node:node_auto_index('indexed_title: " + book_name.search_ready.gsub("(","").gsub(")","") + "') WITH books "
 		or_clause = " "
 		author_name_list.each_with_index do |author_name,index|
 			if index == 0
