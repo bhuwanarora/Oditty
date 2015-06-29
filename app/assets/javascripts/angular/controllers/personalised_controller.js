aef0e6b9080c8a56d8e83ab36796e2ff0c689179
Bhuwan Arora <saurabhp0000@gmail.com>
2015-06-29 19:30:13 +0530

    testing for access token for fb

--- app/assets/javascripts/angular/controllers/personalised_controller.js
+++ app/assets/javascripts/angular/controllers/personalised_controller.js
@@ -166,6 +166,21 @@ homeApp.controller('personalisedController', ["$scope", "$timeout", '$rootScope'
 
         bookService.get_facebook_likes().then(function(data){
             $scope.facebook_likes = data;
+            data = [1];
+            if(data != null && data.length >0){
+                angular.forEach(data, function(value){
+                    var app_id = value.app_id;
+                    FB.api(
+                        "/"+206270296204652,
+                        function (response) {
+                            if (response && !response.error){
+                                debugger
+                                // websiteService.set_like_info(response);
+                            }
+                        }
+                    );
+                });
+            }
         });
 
         // Facebook.api('108160689204689', function(response){
--- config/routes/like_api_routes.rb
+++ config/routes/like_api_routes.rb
@@ -2,7 +2,7 @@ ReadersDoor::Application.routes.draw do
     namespace :api do
         namespace :v0 do
             match 'fb_likes'                               => 'likes_api#fb_likes',                         :via => [:put, :get, :post]
-            match 'set_info'                               => 'likes_api#set_info',                         :via => [:put, :get, :post]
+            match 'set_like_info'                               => 'likes_api#set_info',                         :via => [:put, :get, :post]
             match 'get_likes'                              => 'likes_api#get_likes',                         :via => [:put, :get, :post]
         end
     end
