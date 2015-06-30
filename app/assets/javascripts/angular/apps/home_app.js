d8a39c326e0c2ddfe95867657de557620eeae4cd
Bhuwan Arora <saurabhp0000@gmail.com>
2015-06-30 11:27:52 +0530

    staging

--- app/assets/javascripts/angular/apps/home_app.js
+++ app/assets/javascripts/angular/apps/home_app.js
@@ -79,7 +79,7 @@ homeApp.config(["$mdThemingProvider", function($mdThemingProvider){
                       .warnPalette('googleRed')
 }]);
 
-homeApp.constant('facebookAppId', "667868653261167");
+homeApp.constant('facebookAppId', "742659549115410");
 // 667868653261167
 // 742659549115410
 
