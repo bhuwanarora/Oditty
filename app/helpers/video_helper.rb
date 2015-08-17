b5968bd9cbc9452a3cca7bd38dd2abc7f0c895d1
ashesh-0 <ashesh276@gmail.com>
2015-08-17 16:13:23 +0530

    video links to be corrected

---
+++ app/helpers/video_helper.rb
@@ -0,0 +1,27 @@
+module VideoHelper
+
+	def self.set_up_redis label, key = 'video_duplicate_links_removal'
+		(max_id,min_id) = Neo.get_max_min_id label
+		cur_id = RedisHelper.set_up_redis key, min_id
+		{:cur_id => cur_id, :max_id => max_id}
+	end
+
+	def self.correct_link_has_video params
+		clause = ''\
+				' MATCH (c:Community)<-[hasvideo:HasVideo]-(video:Video) '\
+				' MERGE (c)-[hasvideonew:HasVideo]->(video) '\
+				' ON CREATE SET hasvideonew.rank = hasvideo.rank '\
+				' ON MATCH SET hasvideonew.rank = COALESCE(hasvideonew.rank, hasvideo.rank) '\
+				' RETURN MAX(ID(c)) AS id '
+	end
+
+	def self.repair_video_links_to_community
+		params = {
+			:class 			=> VideoHelper,
+			:label 			=> 'Community',
+			:function 		=> VideoHelper.correct_link_has_video
+			:step_size 		=> 1
+		}
+		GraphHelper.iterative_entity_operations params
+	end
+end
