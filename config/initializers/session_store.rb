c6923010e51d2e463986a18bffba48903a98f45f
ashesh-0 <ashesh276@gmail.com>
2015-09-01 10:10:05 +0530

    added domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-ReadersDoor::Application.config.session_store :cookie_store, key: '_readers_door_session'
+ReadersDoor::Application.config.session_store :cookie_store, key: '_oditty_me_session', domain: 'oditty.me'
