fd272b3d774f59c9238c25285fc7be138ed16a94
Bhuwan Arora <saurabhp0000@gmail.com>
2015-09-03 22:02:20 +0530

    testimonial

--- app/models/users_testimonial.rb
+++ app/models/users_testimonial.rb
@@ -6,7 +6,7 @@ class UsersTestimonial < Neo
 	end
 
 	def create description
-		@user.match + " CREATE UNIQUE (testimonial: Testimonial)-[:WrittenBy]->(user) WITH testimonial, user " + UsersTestimonial.set_description(description) + UsersTestimonial.set_created_at + UsersTestimonial.set_user_id
+		@user.match + " CREATE (testimonial: Testimonial) " + UsersTestimonial.set_description(description) + UsersTestimonial.set_created_at + UsersTestimonial.set_user_id + " CREATE UNIQUE (testimonial)-[:WrittenBy]->(user) "
 	end
 
 	def self.set_description description
