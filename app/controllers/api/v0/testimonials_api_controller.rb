11a31cffd387df310ac88479371d1a793598092d
Bhuwan Arora <saurabhp0000@gmail.com>
2015-09-04 18:56:47 +0530

    testimonial

--- app/controllers/api/v0/testimonials_api_controller.rb
+++ app/controllers/api/v0/testimonials_api_controller.rb
@@ -6,12 +6,17 @@ module Api
 				user_id = session[:user_id]
 				description = params[:data]
 				Api::V0::TestimonialApi.create(user_id, description)
+				RedisHelper::Testimonial.delete_all
 				render :json => {:message => "Success"}, :status => 200
 			end
 
 			def get_testimonials
 				skip = params[:skip]
-				info = Api::V0::TestimonialApi.get_all skip
+				info = RedisHelper::Testimonial.get_all skip
+				if info.nil?
+					info = Api::V0::TestimonialApi.get_all skip
+					RedisHelper::Testimonial.set_all skip
+				end
 				render :json => info, :status => 200
 			end
 		end
