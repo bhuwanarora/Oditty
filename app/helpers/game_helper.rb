db7470f58366b8fff13a2e208e53396e2651b981
Bhuwan Arora <saurabhp0000@gmail.com>
2015-09-05 15:50:04 +0530

    feed

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -3,7 +3,7 @@ module GameHelper
 	def self.create_book_linked_list
 		clause = "MATCH (b:Book) "\
 			" WHERE b.goodness_index > 7 AND b.isbn IS NOT NULL "\
-			" WITH COLLECT(book) AS p "\
+			" WITH COLLECT(b) AS p "\
 			" FOREACH(i in RANGE(0, length(p)-2) | "\
 				" FOREACH(p1 in [p[i]] |  "\
 					" FOREACH(p2 in [p[i+1]] |  "\
