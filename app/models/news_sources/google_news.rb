adf1867fe3adac1bd2a12b159e0d026765afa003
ashesh-0 <ashesh276@gmail.com>
2015-09-23 12:23:49 +0530

    videos not getting added

--- app/models/news_sources/google_news.rb
+++ app/models/news_sources/google_news.rb
@@ -2,7 +2,7 @@ class NewsSources::GoogleNews < NewsSources
 	def self.fetch_news_info
 		news_metadata_array = []
 		news_sources = NewsHelper.fetch_news_sources
-		for news_source in news_sources
+		for news_source in news_sources[0..6]
 			news_links = NewsHelper.fetch_news news_source["url"]
 			puts news_links
 			for news_link in news_links
@@ -12,6 +12,7 @@ class NewsSources::GoogleNews < NewsSources
 				news_metadata["news_link"] = news_link
 				news_metadata["region"] = news_source["region"]
 				news_metadata_array << news_metadata
+				break
 			end
 		end
 		news_metadata_array
