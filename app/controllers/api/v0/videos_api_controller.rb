5ef487613b411901f4538a3b9861519f5a164a64
Bhuwan Arora <saurabhp0000@gmail.com>
2015-10-06 18:35:15 +0530

    genre id

--- app/controllers/api/v0/videos_api_controller.rb
+++ app/controllers/api/v0/videos_api_controller.rb
@@ -3,7 +3,7 @@ module Api
 		class VideosApiController < ApplicationController
 			def add
 				Api::V0::VideoApi.update_community_videos(params[:community_id], params[:videos])
-				render :json => true, :status => 200
+				render :json => true, :status => 200 
 			end
 		end
 	end
